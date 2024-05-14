9749116086149fa39cbc26040088dadabd855237
Martin Yanev <martin.yanev@extreme.consulting>
2024-05-14 09:52:30 +0300

    Mine page implemented

--- HouseRentingSystem/HouseRentingSystem/Controllers/HouseController.cs
+++ HouseRentingSystem/HouseRentingSystem/Controllers/HouseController.cs
@@ -2,16 +2,54 @@
 {
     using Microsoft.AspNetCore.Mvc;
 
+    using Data;
     using Common;
     using Web.ViewModels.House;
+    using Microsoft.AspNetCore.Authorization;
+    using System.Security.Claims;
 
     public class HouseController : Controller
     {
+        private readonly HouseRentingDbContext _dbContext;
+
+        public HouseController(HouseRentingDbContext context)
+        {
+            _dbContext = context;
+        }
+
         public IActionResult All()
         {
             AllHousesViewModel allHouses = new AllHousesViewModel()
             {
-                Houses = RawData.GetHouses()
+                Houses = _dbContext.Houses
+                    .Select(h => new HouseDetailsViewModel()
+                    {
+                        Address = h.Address,
+                        Title = h.Title,
+                        ImageUrl = h.ImageUrl,
+                    })
+            };
+
+            return View(allHouses);
+        }
+
+        [Authorize]
+        public IActionResult Mine()
+        {
+            string? currentUserId = this.User
+                .FindFirst(ClaimTypes.NameIdentifier)?
+                .Value;
+
+            AllHousesViewModel allHouses = new AllHousesViewModel()
+            {
+                Houses = _dbContext.Houses
+                    .Where(h => h.Agent.UserId ==  currentUserId)
+                    .Select(h => new HouseDetailsViewModel()
+                    {
+                        Address = h.Address,
+                        Title = h.Title,
+                        ImageUrl = h.ImageUrl,
+                    })
             };
 
             return View(allHouses);
